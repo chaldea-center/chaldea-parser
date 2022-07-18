620ac01d8a9ab6be9837cd7e8ce49b8f54613835
narumi <yan.jianhui147@gmail.com>
2022-07-19 07:52:34 +0800

    clone api/app instead of installing

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,23 +1,14 @@
 import logging
 from pathlib import Path
 
-from uvicorn.logging import ColourizedFormatter
-
 from ..config import settings
 
 
-class _LogFormatter(ColourizedFormatter):
-    def should_use_colors(self) -> bool:
-        # return sys.stderr.isatty() or hasattr(sys, "ps1")
-        return True
-
-
 logger = logging.getLogger("chaldea-parser")
-_formatter = _LogFormatter(
+_formatter = logging.Formatter(
     fmt="{asctime} [{filename}:{lineno:>3d}] {levelname:<5s}: {message}",
     datefmt="%H:%M:%S",
     style="{",
-    use_colors=True,
 )
 logger.handlers.clear()
 console_handler = logging.StreamHandler()
