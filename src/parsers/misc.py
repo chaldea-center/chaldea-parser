f220e6263e70272ed86c3aaea6f494a6dfc330a0
narumi <yan.jianhui147@gmail.com>
2022-08-25 04:47:55 +0800

    update skill and td names from raw data

--- src/parsers/misc.py
+++ src/parsers/misc.py
@@ -1,25 +1,23 @@
+#%%
+import httpx
 from app.schemas.common import Region
+from app.schemas.raw import MstSkill, MstTreasureDevice
 
 from src.config import settings
 from src.schemas.common import MappingStr
-from src.utils import AtlasApi
-from src.utils.helper import dump_json, load_json, sort_dict
-from src.utils.log import logger
+from src.utils.helper import dump_json, load_json
 
 
-_ce_cc_names: dict[str, MappingStr] = {}
+def _mstFile(region: Region, name: str):
+    url = f"https://git.atlasacademy.io/atlasacademy/fgo-game-data/raw/branch/{region}/master/{name}"
+    print(f"reading: {url}")
+    return httpx.get(url).json()
 
 
 def add_enemy_skill_np_trans():
-    AtlasApi.api_server = "http://127.0.0.1:8000"
-    logger.level = 20
-    _ce_cc_names.clear()
-    _ce_cc_names.update(
-        _load_mapping(settings.output_mapping / "ce_names.json")
-        | _load_mapping(settings.output_mapping / "cc_names.json")
-    )
     _add_enemy_skill_trans()
     _add_enemy_td_trans()
+    print("done")
 
 
 def _load_mapping(fp) -> dict[str, MappingStr]:
@@ -27,66 +25,85 @@ def _load_mapping(fp) -> dict[str, MappingStr]:
 
 
 def _add_enemy_skill_trans():
-    skills: list[dict] = load_json(settings.output_dist / "baseSkills.json") or []
+    skills_jp: list[dict] = load_json(settings.output_dist / "baseSkills.json") or []
     mappings: dict[str, MappingStr] = _load_mapping(
         settings.output_mapping / "skill_names.json"
     )
-    skills = [s for s in skills if s["name"] and s["name"] not in _ce_cc_names]
-    for index, skill in enumerate(skills):
-        skill_id, skill_name = skill["id"], skill["name"]
-        if not str(skill_name).strip() or skill_name in _ce_cc_names:
+    for region in Region.__members__.values():
+        if region == Region.JP:
             continue
-        trans = mappings.setdefault(skill_name, MappingStr())
-        print(f"\rfetch skill {skill_id} ({index}/{len(skills)})", end="")
-        for region in Region.__members__.values():
-            if region == Region.JP:
+        skills_r: dict[int, MstSkill] = {
+            skill["id"]: MstSkill.parse_obj(skill)
+            for skill in _mstFile(region, "mstSkill.json")
+        }
+        for skill_jp in skills_jp:
+            name_jp = skill_jp["name"]
+            if not str(name_jp).strip() or name_jp not in mappings:
                 continue
+            trans = mappings[name_jp]
             if trans.of(region) is not None:
                 continue
-            skill_r = AtlasApi.api_json(
-                f"/nice/{region}/skill/{skill_id}", expire_after=0
-            )
-            if skill_r is None or "id" not in skill_r:
+            skill_r = skills_r.get(skill_jp["id"])
+            if not skill_r:
                 continue
-            if skill_r["name"] != skill_name:
-                trans.update(region, skill_r["name"], True)
-    mappings = sort_dict(mappings)
+            name_r = skill_r.name
+            if not name_r or name_r == name_jp:
+                continue
+            trans.update(region, name_r, True)
     dump_json(mappings, settings.output_mapping / "skill_names.json")
 
 
 def _add_enemy_td_trans():
-    tds: list[dict] = load_json(settings.output_dist / "baseTds.json") or []
-    names: dict[str, MappingStr] = _load_mapping(
+    tds_jp: list[dict] = load_json(settings.output_dist / "baseTds.json") or []
+    td_names: dict[str, MappingStr] = _load_mapping(
         settings.output_mapping / "td_names.json"
     )
-    rubies: dict[str, MappingStr] = _load_mapping(
+    td_rubies: dict[str, MappingStr] = _load_mapping(
         settings.output_mapping / "td_ruby.json"
     )
-    for index, td in enumerate(tds):
-        td_id, td_name, td_ruby = td["id"], td["name"], td["ruby"]
-        if not str(td_name).strip() or td_name in _ce_cc_names:
+    for region in Region.__members__.values():
+        if region == Region.JP:
             continue
-        name_trans = names.setdefault(td_name, MappingStr())
-        ruby_trans = rubies.setdefault(td_ruby, MappingStr())
-        print(f"\rfetch td {td_id} ({index}/{len(tds)})", end="")
-        for region in Region.__members__.values():
-            if region == Region.JP:
+        tds_r: dict[int, MstTreasureDevice] = {
+            td["id"]: MstTreasureDevice.parse_obj(td)
+            for td in _mstFile(region, "mstTreasureDevice.json")
+        }
+        for td_jp in tds_jp:
+            # name
+            name_jp = td_jp["name"]
+            if not str(name_jp).strip() or name_jp not in td_names:
                 continue
-            if name_trans.of(region) is not None:
+            trans = td_names[name_jp]
+            if trans.of(region) is not None:
                 continue
-            td_r = AtlasApi.api_json(f"/nice/{region}/NP/{td_id}", expire_after=0)
-            if td_r is None or "id" not in td_r:
+            td_r = tds_r.get(td_jp["id"])
+            if not td_r:
                 continue
-            if td_r["name"] != td_name:
-                name_trans.update(region, td_r["name"], True)
-            if region != Region.NA and td_r["ruby"] != td_ruby:
-                ruby_trans.update(region, td_r["ruby"], True)
-    names = sort_dict(names)
-    rubies.pop("-", None)
-    rubies = sort_dict(rubies)
-    dump_json(names, settings.output_mapping / "td_names.json")
-    dump_json(rubies, settings.output_mapping / "td_ruby.json")
+            name_r = td_r.name
+            if not name_r or name_r == name_jp:
+                continue
+            trans.update(region, name_r, True)
+        for td_jp in tds_jp:
+            # ruby
+            if region == Region.NA:
+                continue
+            ruby_jp = td_jp["ruby"]
+            if not str(ruby_jp).strip() or ruby_jp not in td_rubies:
+                continue
+            trans = td_rubies[ruby_jp]
+            if trans.of(region) is not None:
+                continue
+            td_r = tds_r.get(td_jp["id"])
+            if not td_r:
+                continue
+            ruby_r = td_r.ruby
+            if not ruby_r or ruby_r == ruby_jp or ruby_r == "-":
+                continue
+            trans.update(region, ruby_r, True)
+    dump_json(td_names, settings.output_mapping / "td_names.json")
+    dump_json(td_rubies, settings.output_mapping / "td_ruby.json")
 
 
+#%%
 if __name__ == "__main__":
     add_enemy_skill_np_trans()
