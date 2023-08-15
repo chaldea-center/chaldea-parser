f41c598e48541559e00b6628da879914782b1f51
narumi <yan.jianhui147@gmail.com>
2023-08-15 16:27:53 +0800

    Skip wiki parser if mapping changed only

--- main.py
+++ main.py
@@ -11,6 +11,44 @@ from src.wiki import FANDOM, MOONCELL  # noqa
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+
+def should_run_wiki_parser(payload: PayloadSetting):
+    print("Payload:", payload.json(exclude_unset=True, indent=2), flush=True)
+    output_dir = Path(settings.output_dir).resolve()
+    authors = subprocess.check_output(
+        "git log -2 --pretty=format:'%an'",
+        shell=True,
+        text=True,
+        cwd=output_dir,
+    ).splitlines()
+    changed_files = subprocess.check_output(
+        "git diff-tree --no-commit-id --name-only HEAD -r",
+        shell=True,
+        text=True,
+        cwd=output_dir,
+    )
+    print("Last Authors:", authors, flush=True)
+    print("Commit Files:", changed_files, flush=True)
+    assert authors and changed_files, "No info in authors or changed_files"
+    if payload.run_wiki_parser is not None:
+        print(f"Forced run_wiki_parser={payload.run_wiki_parser}", flush=True)
+        return payload.run_wiki_parser
+
+    if "github-actions[bot]" in authors[0]:
+        print("bot changes, skip wiki parser", flush=True)
+        return False
+    if len(authors) > 1 and "github-actions[bot]" not in authors[1]:
+        print("More than 1 user commit, run wiki parser", flush=True)
+        return True
+
+    for filename in changed_files.splitlines():
+        if filename and not filename.startswith("mappings/"):
+            print("Detected changes outside mappings/ folder", flush=True)
+            return True
+    print("Only mapping file changed, skip wiki parser", flush=True)
+    return False
+
+
 # %%
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
@@ -32,19 +70,7 @@ if __name__ == "__main__":
         wiki_parser.start()
         # run_wiki_parser()
     elif task == "trywiki":
-        summary = subprocess.check_output(
-            "git log -1 --pretty=format:'%an'",
-            shell=True,
-            text=True,
-            cwd=Path(settings.output_dir).resolve(),
-        )
-        print("Last Author:", summary, flush=True)
-        print("Payload:", payload.json(exclude_unset=True, indent=2), flush=True)
-        if payload.run_wiki_parser is not None:
-            should_run = payload.run_wiki_parser
-        else:
-            should_run = "github-actions[bot]" not in summary
-        if should_run:
+        if should_run_wiki_parser(payload):
             wiki_parser = WikiParser()
             wiki_parser.start()
         else:
