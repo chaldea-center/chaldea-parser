09a7ad12afe23fb58fdea8b6127858411f7a9ffb
narumi <yan.jianhui147@gmail.com>
2026-05-25 00:55:29 +0800

    Update packages

--- scripts/update_api_dep.sh
+++ scripts/update_api_dep.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 pip uninstall --yes fgo-game-data-api && poetry update fgo-game-data-api
-poetry export --without-hashes -f requirements.txt -o requirements.txt
+# poetry self add poetry-plugin-export
+# poetry export --without-hashes --without-urls -f requirements.txt -o requirements.txt
